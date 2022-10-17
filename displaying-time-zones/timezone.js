4485d8341726d2f4f802bbc4ba47ad65f5899ec5
Wajid Hussain <wajidhussain1@live.com>
2022-10-17 19:35:49 +0100

    Fixed a minor mistake
    I noticed a capital letter in a variable, that if not changed would break the script.

--- displaying-time-zones/timezone.js
+++ displaying-time-zones/timezone.js
@@ -15,11 +15,12 @@
 
     LTime = new Date();
     LHour = LTime.getHours();
+    console.log(LHour)
 
     if (LHour > 7 && LHour < 22) {
         document.getElementById("londonClockSVG").innerHTML = '<svg style= "width: 25px; height: 25px; padding-top: 15px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 12v-6h-2v8h7v-2h-5z"/></svg>';
     } else {
-        document.getElementById("LondonClockSVG").innerHTML = '<svg style= "width: 25px; height: 25px; padding-top: 15px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 14h-7v-8h2v6h5v2z"/></svg>';
+        document.getElementById("londonClockSVG").innerHTML = '<svg style= "width: 25px; height: 25px; padding-top: 15px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 14h-7v-8h2v6h5v2z"/></svg>';
     }
 
     getNewYorkTime();
